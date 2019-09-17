9edc65fa3c8d3c8c68fd9807edd83eb4a157aaee
Rhythm Fox <rhythm@pop-os.localdomain>
2019-09-16 20:48:06 -0400

    customized styles of the Sign Out button (added pill border and implemented flex box with Tachyons)

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'> Sign Out </p>
+        <nav className='flex justify-end'>
+            <p className='f3 ba br-pill link purple dim black underline pa3 pointer'> Sign Out </p>
         </nav>
     );
 }
