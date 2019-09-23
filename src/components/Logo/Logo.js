5a1895c30e4f0872a7040093c47113830f39fda4
Rhythm Fox <rhythm@pop-os.localdomain>
2019-09-23 19:27:00 -0400

    Created Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Logo.css'
+import Tilt from 'react-tilt'
+import foxImg from './icons8-fox-32.png';
+
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className='Tilt shadow-2' options={{ max : 25, perspective : 750, speed : 150, reverse: true }} style={{ height: 250, width: 250 }} >
+                <div className='Tilt-inner pa3'>
+                    <img style={{ height: '50px', width: '50px'}} src={foxImg} alt='minimal fox head'></img>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
