04babc23626665c01f418be3d876b1a1496b1488
jjrobertson14 <johnathon.j.robertson@gmail.com>
2019-09-23 19:27:00 -0400

    Created Logo component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, {Component} from 'react';
 import 'tachyons';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navigation />
-        {/*
-          <Logo />
-          <ImageLinkForm />
-          <FaceRecognition />
-        */}
+        <Logo />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
