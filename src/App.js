ccd4a7061789ece1d0ece715541eb993dfa78be8
Rhythm Fox <rhythm@pop-os.localdomain>
2019-09-25 20:31:13 -0400

    Added Rank component and react-particles-js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import 'tachyons';
+import Particles from 'react-particles-js';
+import particlesOptions from './particlesjs-config.js'
 import './App.css';
-import Navigation from './components/Navigation/Navigation.js'
-import Logo from './components/Logo/Logo.js'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import Rank from './components/Rank/Rank.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
+        <Particles 
+          className='particles'
+          params={particlesOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
