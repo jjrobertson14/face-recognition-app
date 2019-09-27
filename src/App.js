bcdc2b82c31485462681978e3a51b04192532afb
jjrobertson14 <johnathon.j.robertson@gmail.com>
2019-09-26 20:43:20 -0400

    Added Face Recognition Functionality

--- src/App.js
+++ src/App.js
@@ -7,8 +7,38 @@ import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '8e1cd203cf7c44e29fdf2849cf330ba4',
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = (event) => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[2].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className='App'>
@@ -18,8 +48,13 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition 
+          imageUrl={this.state.imageUrl}
+        />
       </div>
     );
   }
