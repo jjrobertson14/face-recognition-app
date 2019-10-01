d7ad027845a176f334b9a9556a01b8d54c3c99c3
Rhythm Fox <rhythm@pop-os.localdomain>
2019-09-30 21:06:55 -0400

    added some example urls that point to images with faces to detect

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,11 @@ const FaceRecognition = ({ box, imageUrl }) => {
         <div className='center ma'>
             <div className='absolute, mt2, relative'>
                 <img id='input-image' alt='' src={ imageUrl } style={{width: '500px', height: 'auto'}} />
-                {/* to try this out: https://samples.clarifai.com/face-det.jpg */}
+                {/* urls to try this out with: 
+                https://samples.clarifai.com/face-det.jpg 
+                https://media.self.com/photos/5b64a1ee56b2e2706f75579f/4:3/w_752,c_limit/bo-bridges-bike-shot.jpg
+                https://s3-us-west-2.amazonaws.com/flx-editorial-wordpress/wp-content/uploads/2019/07/08154231/700JW3.jpg
+                */}
                 <div className='bounding-box' style={{left: box.leftCol, top: box.topRow, right: box.rightCol, bottom: box.bottomRow}} />
             </div>
         </div>
