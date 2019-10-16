230cba3c7d110a9066a423863da21b07e6bc1039
jjrobertson14 <johnathon.j.robertson@gmail.com>
2019-10-16 18:01:22 -0400

    added signin POST endpoint

--- api/server.js
+++ api/server.js
@@ -1,14 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+// testing mock
+const database = {
+    users: [
+        {
+            id: 1,
+            name: 'Johnathon',
+            email: 'jrob@mail.org',
+            password: 'hashed-I-swear',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: 2,
+            name: 'Smart Fox',
+            email: 'fox@gmail.com',
+            password: 'xof123gsdfgoj_+!',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: 3,
+            name: 'Joey',
+            email: 'joey@mail.org',
+            password: 'super-hashed',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.listen(3000, () => {
     console.log("hello there, I am the express monkey"); 
 });
 
-app.get('/', (req, res) => {
-    res.send('this is working');
-});
 
 /*
 / --> res = 'this is working'
@@ -18,3 +47,20 @@ app.get('/', (req, res) => {
 /image --> PUT --> user, put in a score increment for a user's account
 */
 
+// --> res = 'this is working'
+app.get('/', (req, res) => {
+    res.send('this is working');
+});
+
+//signin --> POST credentials = success / fail
+app.post('/signin', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    if (email && password) {
+        if (email === database.users[0].email && password === database.users[0].password) {
+            res.json('success');
+            return;
+        }
+    }
+    res.status(400).json('failure');
+})
